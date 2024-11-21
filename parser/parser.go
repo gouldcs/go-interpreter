c7fcf95ccfd3415e29b20eaec2d7dac1e088f8ce
Cameron Gould <cgould18@georgefox.edu>
2024-11-21 14:45:07 -0800

    Basic return statement parser support without expressions

--- parser/parser.go
+++ parser/parser.go
@@ -56,6 +56,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -75,6 +77,19 @@ func (p *Parser) parseLetStatement() ast.Statement {
 	}
 
 	// TODO: skip expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() ast.Statement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: skipping expressions until we hit semicolon.
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
